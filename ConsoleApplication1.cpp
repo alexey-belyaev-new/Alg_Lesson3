283120c86f7c098887309a7c46c4e5987fe00522
alexey-belyaev-new <78504218+alexey-belyaev-new@users.noreply.github.com>
2022-09-19 20:40:46 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void isPrime(int a) {
+
+	bool prime{};
+	
+	for (int i = 2; i * i <= a; i++)
+	{
+		if (a % i == 0) {			
+			prime = false;
+			break;
+		}
+		prime = true;
+	}
+	
+	if (prime == true) cout << a << " is a prime number." << endl;
+	else cout << a << " not prime number." << endl;
+}
+
+int main()
+{
+	int n;
+	cout << "Enter number: "; cin >> n;
+	isPrime(n);
+
+}
